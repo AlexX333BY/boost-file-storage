985d47a309644763a1b5810f17d3ceec772abf06
Aliaksei <alexx333by@protonmail.com>
2019-02-26 16:52:06 +0300

    Adding files in queue implemented

--- BoostFileStorage/client/FileStorageFrame.cpp
+++ BoostFileStorage/client/FileStorageFrame.cpp
@@ -1,5 +1,6 @@
 #include "FileStorageFrame.h"
 #include "AddressChooserDialog.h"
+#include <wx/dir.h>
 
 namespace boost_file_storage
 {
@@ -7,13 +8,13 @@ namespace boost_file_storage
 	const wxString m_disconnectcCaption = "Disconnect";
 
 	FileStorageFrame::FileStorageFrame(const wxString& title, const int border)
-		: wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE | wxFULL_REPAINT_ON_RESIZE)
+		: wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE | wxFULL_REPAINT_ON_RESIZE), m_socket(nullptr)
 	{
 		wxStatusBar *statusBar = CreateStatusBar();
 		wxBoxSizer *statusBarSizer = new wxBoxSizer(wxHORIZONTAL);
 
 		m_sendingFileGauge = new wxGauge(statusBar, wxID_ANY, 0, wxDefaultPosition, wxSize(statusBar->GetSize().GetWidth() / 10, wxDefaultSize.GetHeight()), wxGA_HORIZONTAL);
-		m_sendingFileName = new wxStaticText(statusBar, wxID_ANY, "");
+		m_sendingFileName = new wxStaticText(statusBar, wxID_ANY, wxEmptyString);
 		m_sendingFileGauge->Show(false);
 		m_sendingFileName->Show(false);
 
@@ -23,20 +24,65 @@ namespace boost_file_storage
 		statusBar->Layout();
 
 		wxPanel *panel = new wxPanel(this);
-		m_connectButton = new wxButton(panel, wxID_ANY, m_connectCaption);
-		const wxString addFileCaption = "Add file in queue";
-		m_addFileButton = new wxButton(panel, wxID_ANY, addFileCaption);
+		m_connectButton = new wxButton(panel, m_connectButtonId, m_connectCaption);
+		const wxString addFileCaption = "Add file in queue", addFolderCaption = "Add folder in queue";
 
 		wxBoxSizer *buttonsSizer = new wxBoxSizer(wxHORIZONTAL);
 		buttonsSizer->Add(m_connectButton, 0, wxALL | wxALIGN_LEFT, border);
-		buttonsSizer->Add(m_addFileButton, 0, wxALL | wxALIGN_LEFT, border);
+		buttonsSizer->Add(new wxButton(panel, m_addFileButtonId, addFileCaption), 0, wxALL | wxALIGN_LEFT, border);
+		buttonsSizer->Add(new wxButton(panel, m_addFolderButtonId, addFolderCaption), 0, wxALL | wxALIGN_LEFT, border);
 
-		m_fileList = new wxListBox(panel, wxID_ANY);
+		m_log = new wxListBox(panel, wxID_ANY);
 
 		wxBoxSizer *frameSizer = new wxBoxSizer(wxVERTICAL);
 		frameSizer->Add(buttonsSizer, 0, wxALL | wxALIGN_LEFT, border);
-		frameSizer->Add(m_fileList, 1, wxALL | wxEXPAND, border);
+		frameSizer->Add(m_log, 1, wxALL | wxEXPAND, border);
 
 		panel->SetSizer(frameSizer);
+
+		Bind(wxEVT_BUTTON, &FileStorageFrame::OnFileAdd, this);
+		Bind(wxEVT_BUTTON, &FileStorageFrame::OnFolderAdd, this);
+	}
+
+	void FileStorageFrame::Log(const wxString *messages, unsigned int count)
+	{
+		m_log->InsertItems(count, messages, m_log->GetCount());
+	}
+
+	void FileStorageFrame::OnFileAdd(wxCommandEvent& event)
+	{
+		if (event.GetId() == m_addFileButtonId)
+		{
+			wxFileDialog fileDialog(this, wxFileSelectorPromptStr, wxEmptyString, wxEmptyString, wxFileSelectorDefaultWildcardStr,
+				wxFD_DEFAULT_STYLE | wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_MULTIPLE);
+			if (fileDialog.ShowModal() == wxID_OK)
+			{
+				wxArrayString paths;
+				fileDialog.GetPaths(paths);
+				for (size_t i = 0; i < paths.GetCount(); ++i)
+				{
+					m_filesQueue.push(paths[i]);
+					Log(&m_logGenerator.GenerateAddFileMessage(paths[i]));
+				}
+			}
+		}
+	}
+
+	void FileStorageFrame::OnFolderAdd(wxCommandEvent& event)
+	{
+		if (event.GetId() == m_addFolderButtonId)
+		{
+			wxDirDialog dirDialog(this, wxDirSelectorPromptStr, wxEmptyString, wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
+			if (dirDialog.ShowModal() == wxID_OK)
+			{
+				wxArrayString files;
+				wxDir::GetAllFiles(dirDialog.GetPath(), &files, wxEmptyString, wxDIR_FILES | wxDIR_DIRS | wxDIR_NO_FOLLOW);
+				for (size_t i = 0; i < files.GetCount(); ++i)
+				{
+					m_filesQueue.push(files[i]);
+					Log(&m_logGenerator.GenerateAddFileMessage(files[i]));
+				}
+			}
+		}
 	}
 }
--- BoostFileStorage/client/FileStorageFrame.h
+++ BoostFileStorage/client/FileStorageFrame.h
@@ -6,7 +6,9 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
+#include <queue>
 #include "../common/client_socket.h"
+#include "LogMessagesGenerator.h"
 
 namespace boost_file_storage
 {
@@ -15,11 +17,19 @@ namespace boost_file_storage
 	public:
 		FileStorageFrame(const wxString& title, const int border = 5);
 	protected:
-		client_socket *socket;
-		wxArrayString files_queue;
+		LogMessagesGenerator m_logGenerator;
+		void Log(const wxString *messages, unsigned int count = 1);
+		client_socket *m_socket;
+		std::queue<wxString> m_filesQueue;
+
 		wxGauge *m_sendingFileGauge;
-		wxButton *m_connectButton, *m_addFileButton;
-		wxListBox *m_fileList;
+		wxButton *m_connectButton;
+		wxListBox *m_log;
 		wxStaticText *m_sendingFileName;
+		
+		void OnFileAdd(wxCommandEvent& event);
+		void OnFolderAdd(wxCommandEvent& event);
+
+		const int m_addFileButtonId = wxID_ADD, m_addFolderButtonId = wxID_ADD + 1, m_connectButtonId = wxID_NETWORK;
 	};
 }
