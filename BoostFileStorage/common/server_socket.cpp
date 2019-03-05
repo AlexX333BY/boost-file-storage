43aa0e5bd532fb228b53bb67ae0b4db1cd5fbca9
unknown <--help>
2019-03-05 11:28:42 +0300

    Revert "Replaced pointers with simple instances"
    This reverts commit 7b7f0d62f72a29e1e59e667be8adaea4439a8b38.
    
    Wrong branch

--- BoostFileStorage/common/server_socket.cpp
+++ BoostFileStorage/common/server_socket.cpp
@@ -3,9 +3,11 @@
 namespace boost_file_storage
 {
 	server_socket::server_socket(unsigned short port, size_t desired_buffer_size)
-		: socket(), m_state(CLOSED), m_context(), m_acceptor(m_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+		: socket(), m_state(CLOSED), m_context(new boost::asio::io_context())
 	{
 		m_tcp_socket = nullptr;
+		boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
+		m_acceptor = new boost::asio::ip::tcp::acceptor(*m_context, endpoint);
 		m_buffer_size = std::max(std::max(sizeof(message_type), sizeof(size_t)), desired_buffer_size);
 	}
 
@@ -17,7 +19,10 @@ namespace boost_file_storage
 		}
 
 		boost::system::error_code error;
-		m_acceptor.close(error);
+		m_acceptor->close(error);
+
+		delete m_acceptor;
+		delete m_context;
 	}
 
 	boost::system::error_code server_socket::open()
@@ -25,7 +30,7 @@ namespace boost_file_storage
 		if (is_closed())
 		{
 			boost::system::error_code error;
-			m_tcp_socket = new boost::asio::ip::tcp::socket(m_context);
+			m_tcp_socket = new boost::asio::ip::tcp::socket(*m_context);
 			m_state = OPENED;
 			return error;
 		}
@@ -35,22 +40,27 @@ namespace boost_file_storage
 		}
 	}
 
+	void server_socket::accept_handler(const boost::system::error_code& error)
+	{
+		m_accept_error = error;
+	}
+
 	boost::system::error_code server_socket::accept()
 	{
 		if (is_opened())
 		{
 			boost::system::error_code error;
-			m_acceptor.wait(boost::asio::ip::tcp::acceptor::wait_read, error);
+			m_acceptor->wait(boost::asio::ip::tcp::acceptor::wait_read, error);
 			if (!error)
 			{
-				m_acceptor.accept(*m_tcp_socket, error);
+				m_acceptor->accept(*m_tcp_socket, error);
 				if (!error)
 				{
 					m_state = CONNECTED;
 				}
 			}
 
-			return error;
+			return error ? error : m_accept_error;
 		}
 		else if (is_connected())
 		{
@@ -67,7 +77,7 @@ namespace boost_file_storage
 		boost::system::error_code error;
 		if (!is_closed())
 		{
-			m_acceptor.cancel(error);
+			m_acceptor->cancel(error);
 			m_tcp_socket->cancel(error);
 			m_tcp_socket->shutdown(m_tcp_socket->shutdown_both, error);
 			m_tcp_socket->close(error);
